1fa8bde152b5f1d692fee496a202be429533841a
Soufiane Chaghil <soufianechaghil@gmail.com>
2024-01-01 15:53:50 +0100

    add: dev

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="text-center mt-10">
-      Central Taxi 67
+      Central Taxi 67 on developement
     </main>
   )
 }
