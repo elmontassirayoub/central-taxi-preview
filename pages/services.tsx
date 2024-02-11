e75736ccc5381146f8ef8ad756fe640315cf03ef
Soufiane Chaghil <soufianechaghil@gmail.com>
2024-02-11 16:24:30 +0100

    add: services page

--- pages/services.tsx
+++ pages/services.tsx
@@ -7,6 +7,11 @@ import { useEffect, useState } from "react";
 import Logo from '@/assets/logo.png'
 import Data from '@/assets/data'
 import Footer from "@/components/users/elements/Footer";
+import AddRoadIcon from '@mui/icons-material/AddRoad';
+import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
+import TrainIcon from '@mui/icons-material/Train';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import MapIcon from '@mui/icons-material/Map';
 
 export const getServerSideProps = checkAuthentication(async (context: any, admin: boolean) => {
     return {
@@ -29,6 +34,14 @@ export default function Services({ admin = false }: { admin: boolean }) {
         setLang(val);
         localStorage.setItem("lang", val)
     }
+
+    const services = [
+        { icon: AddRoadIcon, text: "Taxi Strasbourg longue ou courte distance" },
+        { icon: AirplanemodeActiveIcon, text: "Taxi Aéroport Strasbourg Provence" },
+        { icon: TrainIcon, text: "Taxi Strasbourg gare" },
+        { icon: LocalShippingIcon, text: "Service de livraison à Strasbourg en taxi" },
+        { icon: MapIcon, text: "Chauffeur guide touristique Marseille" },
+    ]
 
     const pageData = Data[lang]
 
@@ -53,9 +66,22 @@ export default function Services({ admin = false }: { admin: boolean }) {
                     </div>
                 </div>
             </section>
-            <section className="my-10">
+            <section className="my-10 px-10 lg:px-0">
                 <h1 className="mb-10 lg:mb-0 uppercase text-[24px] lg:text-[40px] lg:[word-spacing:5px] font-meduim text-center text-white">Ce que nous offrons</h1>
                 <h2 className="text-[#fff]/[70%] text-[18px] lg:text-[18px] italic text-center">Nous avons beaucoup d'offres parmi lesquelles vous pouvez choisir</h2>
+                <div className="text-white flex flex-col md:flex-row py-10 px-0 lg:p-20 gap-10">
+                    {
+                        services?.map((service, index) => <div key={index} className="flex-1 border-[1px] text-center p-5 rounded-xl border-secondary-1 flex flex-col gap-5 justify-start items-center">
+                            <div className="bg-white text-secondary-1 p-2 flex items-center justify-center rounded-[50%]">
+                                <service.icon />
+                            </div>
+                            <p>{service.text}</p>
+                        </div>)
+                    }
+                </div>
+                <div className="flex justify-center py-10">
+                    <Call />
+                </div>
             </section>
         </section>
         <Footer lang={lang} changeLanguage={changeLanguage} />
