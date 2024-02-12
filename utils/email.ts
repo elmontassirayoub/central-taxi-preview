222438dab5dca03b76b3b483f30c21f126427322
Soufiane Chaghil <soufianechaghil@gmail.com>
2024-02-12 21:55:16 +0100

    fix: update env var name 3

--- utils/email.ts
+++ utils/email.ts
@@ -14,7 +14,7 @@ export default async function sendMail({to, admin, confirmation, status , reserv
 
     const text = admin ? "Reservation" :  `Madame, Monsieur, \n\nPar la présente, ${status === "pending" ? "je me permets de vous informer qu'on a bien reçu " : `je me permets de vous ${status === "confirmed" ? "confirmer" : "annuler"}`} la réservation de votre taxi en date du ${reservation.date.split('T')[0]} ${reservation.time} , de ${reservation.from} à destination de ${reservation.to}. \n\nSalutations les plus distinguées de la part de toute l'equipe CENTRAL TAXI 67 . \nCordialement CENTRAL TAXI 67`
 
-    const html = (confirmation || !admin) ? "" : `<h3>Nouvelle reservation confirmer or annuler <a style='text-decoration: underline; color: blue;' href='${process.env.NEXT_PUBLIC_APP_URL}/admin/${reservation?._id}'>Go here</a></h3>`
+    const html = (confirmation || !admin) ? "" : `<h3>Nouvelle reservation confirmer or annuler <a style='text-decoration: underline; color: blue;' href='${process.env.NEXTAUTH_URL}/admin/${reservation?._id}'>Go here</a></h3>`
 
     const mailBaseOptions = { from: process.env.EMAIL, to, subject, text }
     const  mailOptions = (confirmation || !admin) ? {...mailBaseOptions} : {...mailBaseOptions, html}
