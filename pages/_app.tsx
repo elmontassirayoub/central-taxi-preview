05010b849ea1f4030c720f0620203b5ebb6c6a5e
Soufiane Chaghil <soufianechaghil@gmail.com>
2024-02-13 14:37:57 +0100

    add: meta tag for description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ function MyApp({ Component, pageProps }: { Component: any, pageProps: any }) {
     <SessionProvider session={pageProps.session}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+        <meta name="description" content="Taxi Strasbourg Services" />
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.4.0/dist/leaflet.css" />
         <title>Taxi Strasbourg Services</title>
       </Head>
