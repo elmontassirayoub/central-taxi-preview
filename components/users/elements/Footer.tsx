d71e0ada83ba57c4f5c44297f381dac4c7a10764
Soufiane Chaghil <soufianechaghil@gmail.com>
2024-02-24 12:21:25 +0100

    fix: footer padding

--- components/users/elements/Footer.tsx
+++ components/users/elements/Footer.tsx
@@ -59,7 +59,7 @@ export default function Footer({ lang, changeLanguage }: { lang: string, changeL
                 <p className="text-[#fff]/[70%] lg:text-center text-xs">Nous sommes équipés de TPE pour le paiement par Carte bancaire.</p>
             </div>
         </section>
-        <section className="min-h-10 bg-[#000] px-10 py-2 lg:py-0    flex lg:items-center flex-col lg:flex-row gap-2 lg:gap-5">
+        <section className="min-h-10 bg-[#000] px-10 py-2 lg:py-0 flex lg:items-center flex-col lg:flex-row gap-2 lg:gap-5 justify-end">
             <p className="text-[#fff]/[70%] text-sm">©Taxi Strasbourg Service. Tous droit réservés.</p>
             <Link href="/legal/terms-of-use" className="underline lg:text-sm text-xs">Conditions général de vente.</Link>
         </section>
