ca4bcbe4e15d8f18537e592ab3473acff269a520
Soufiane Chaghil <soufianechaghil@gmail.com>
2024-02-26 18:03:25 +0100

    fix: error build

--- components/users/elements/Footer.tsx
+++ components/users/elements/Footer.tsx
@@ -49,7 +49,7 @@ export default function Footer({ lang, changeLanguage }: { lang: string, changeL
             <div className="w-full flex-1 text-[#000] flex flex-col lg:items-center gap-5 border-t-[0.5px] border-secondary-100 pt-5 lg:pt-0 lg:border-[0px]">
                 <Review btn={compData.footer.btn1} />
                 <Book btn={compData.footer.btn2} primary={false} />
-                <p className="text-[#fff]/[70%] lg:text-center text-xs">Nous sommes équipés de TPE pour le paiement par Carte bancaire.</p> */}
+                <p className="text-[#fff]/[70%] lg:text-center text-xs">Nous sommes équipés de TPE pour le paiement par Carte bancaire.</p>
             </div>
         </section>
         <section className="min-h-10 bg-[#000] px-10 py-2 lg:py-0 flex lg:items-center flex-col lg:flex-row gap-2 lg:gap-5 justify-end">
