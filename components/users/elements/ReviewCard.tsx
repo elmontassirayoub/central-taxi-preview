028aef9d7ad1030d27931462cf8ca4e902ef6a24
Soufiane Chaghil <soufianechaghil@gmail.com>
2024-03-08 20:30:40 +0100

    fix: phone animation

--- components/users/elements/ReviewCard.tsx
+++ components/users/elements/ReviewCard.tsx
@@ -17,7 +17,7 @@ const ReviewCard: React.FC<ReviewCardProps> = ({ idx, review, name, date, rating
       });
 
     return (
-        <div ref={ref} className={`bg-[#33475A] p-5 rounded-[5px] text-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] flex-1 h-auto flex flex-col justify-between gap-10 hover:md:-translate-y-3 md:ease-in-out transition ${inView ? idx % 2 === 0 ? 'slide_in_left' : "slide_in_right" : ''}`}>
+        <div ref={ref} className={`bg-[#33475A] p-5 rounded-[5px] text-white shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] flex-1 h-auto flex flex-col justify-between gap-10 hover:md:-translate-y-3 md:ease-in-out transition ${inView ? idx % 2 === 0 ? 'slide_in_left' : "slide_in_right" : 'invisible'}`}>
             <p className="lg:text-base text-sm text-white/[85%] flex-1">&quot;{review}&quot;</p>
             <div className="flex justify-between">
                 <div>
