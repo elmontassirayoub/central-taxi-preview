17e5a1559f75e73cda222caa52052ede85b568c6
Soufiane Chaghil <soufianechaghil@gmail.com>
2024-04-03 21:11:22 +0000

    fix: build error

--- pages/tarifs.tsx
+++ pages/tarifs.tsx
@@ -1,6 +1,5 @@
 import PageLayout from "@/components/common/PageLayout";
 import checkAuthentication from "@/lib/middlewares/checkAuthenticated";
-import { useEffect, useState } from "react";
 
 export const getServerSideProps = checkAuthentication(async (context: any, admin: boolean) => {
     return {
@@ -11,20 +10,7 @@ export const getServerSideProps = checkAuthentication(async (context: any, admin
 })
 
 export default function Tarifs({admin}: {admin: boolean}) {
-
-    const [lang, setLang] = useState("fr")
-
-    useEffect(() => {
-        const storedLang = localStorage.getItem("lang")
-        if (storedLang && storedLang !== lang) setLang(storedLang)
-    }, [])
-
-    const changeLanguage = (val: string) => {
-        setLang(val);
-        localStorage.setItem("lang", val)
-    }
-
-    return <PageLayout lang={lang} changeLanguage={changeLanguage} page="/terms-of-use" admin={admin}>
+    return <PageLayout page="/terms-of-use" admin={admin}>
         Tarifs
     </PageLayout>
 }
