0848ae227700f8d9b3bc3f7d9496f9ba5d3ae967
Soufiane Chaghil <soufianechaghil@gmail.com>
2024-04-13 01:32:59 +0000

    add: language i18n

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    i18n: {
+        locales: ["fr"],
+        defaultLocale: "fr",
+    },
     images: {
         domains: ["logo.gralon.net"]
     }
